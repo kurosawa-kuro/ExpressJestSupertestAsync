128f84c053225a51bbd362f3e0ea6e7a63674e18
Toshifumi Kurosawa <kuromailserver@gmail.com>
2019-01-05 01:47:35 +0900

    asyncでもテストが動作するようになった

---
+++ routes/async.js
@@ -0,0 +1,16 @@
+const router = require('express-promise-router')();
+const commonModule = require('../lib/commonModule');
+
+
+router.get('/', async function (req, res) {
+  const param = { 'result': 'async' };      // レスポンスで返す値。JSON形式。
+
+  console.log('Oh time flies so fast...');
+  await commonModule.sleep(3000);
+  console.log('10 seconds passed.');
+
+  res.header('Content-Type', 'application/json; charset=utf-8'); // 「レスポンスはJSON形式で返すよ」の意味
+  res.send(param);
+});
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ const app = express()
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!')
 })
+
+app.use('/', (() => {
+    const router = express.Router();
+    router.use('/async', require('../routes/async'));
+  
+    return router;
+  })());
 module.exports = app
