381be0059d2139d5f808fdc92f2f5f180bbdb300
Toshifumi Kurosawa <kuromailserver@gmail.com>
2019-01-05 02:31:42 +0900

    testコード内でsleep処理成功

--- test/05_AsyncAwait.test.js
+++ test/05_AsyncAwait.test.js
@@ -1,13 +1,18 @@
 const request = require('supertest');
 const app = require('../src/app')
 
+const commonModule = require('../lib/commonModule');
+
 describe('Test the root path', () => {
     beforeEach(() => {
-        jest.setTimeout(10000);
+        jest.setTimeout(30000);
     });
-    
+
     test('It should response the GET method', async () => {
         const response = await request(app).get('/async');
+        console.log("before await commonModule.sleep(6000)")
+        await commonModule.sleep(6000);
+        console.log("after await commonModule.sleep(6000)")
         expect(response.statusCode).toBe(200);
     });
 })
